0c760be7b2aa26b7801e18f0aa363ea45a37324f
Tom Furlong <tom.furlong@ucdconnect.ie>
2022-03-29 18:20:25 +0100

    Started to create main menu

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody my_Rigidbody;
+
+    public float gravityScale = 20;
+    public float jumpHeight = 1;
+    public new Rigidbody rigidbody;
+    public float buttonTime = 0.3f;
+    public float jumpAmount = 20;
+    float jumpTime;
+    bool jumping;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,15 +19,26 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * gravityScale));
         if (Input.GetKey("left") || Input.GetKey("right"))
         {
             Vector3 my_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            my_Rigidbody.MovePosition(transform.position + my_Input * Time.deltaTime * 5f);
+            rigidbody.MovePosition(transform.position + my_Input * Time.deltaTime * 5f);
         }
         if (Input.GetKey("space"))
         {
-            Vector3 my_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            my_Rigidbody.MovePosition(transform.position + my_Input * Time.deltaTime * 5f);
+            jumping = true;
+            jumpTime = 0;
+        }
+        if (jumping)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpAmount);
+            jumpTime += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) | jumpTime > buttonTime)
+        {
+            jumping = false;
         }
     }
 }
+
